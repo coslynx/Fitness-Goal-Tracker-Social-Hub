c5b57896845138b8b6ea9711b26986e9894edc46
CosLynx AI <drixisgod@gmail.com>
2024-10-12 23:27:32 +0530

    generated file: `fitness-tracker/src/shared/ui-components/src/index.ts`

---
+++ fitness-tracker/src/shared/ui-components/src/index.ts
@@ -0,0 +1,6 @@
+import { Button } from './Button';
+import { Card } from './Card';
+import { Input } from './Input';
+import { Typography } from './Typography';
+
+export { Button, Card, Input, Typography };
