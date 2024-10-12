163dafa908906a646b0327d2a131bde61502a0c8
CosLynx AI <drixisgod@gmail.com>
2024-10-12 23:34:52 +0530

    generated file: `fitness-tracker/src/frontend/app/src/components/index.ts`

---
+++ fitness-tracker/src/frontend/app/src/components/index.ts
@@ -0,0 +1,35 @@
+import { Button } from './atoms/Button';
+import { Card } from './atoms/Card';
+import { Input } from './atoms/Input';
+import { Typography } from './atoms/Typography';
+import { FormField } from './molecules/FormField';
+import { NavItem } from './molecules/NavItem';
+import { Header } from './organisms/Header';
+import { Footer } from './organisms/Footer';
+import { Sidebar } from './organisms/Sidebar';
+import { DashboardLayout } from './templates/DashboardLayout';
+import { AuthLayout } from './templates/AuthLayout';
+import { Toast } from './organisms/Toast';
+import { GoalCard } from './molecules/GoalCard';
+import { PostCard } from './molecules/PostCard';
+import { SocialFeed } from './organisms/SocialFeed';
+import { GoalList } from './organisms/GoalList';
+
+export {
+  Button,
+  Card,
+  Input,
+  Typography,
+  FormField,
+  NavItem,
+  Header,
+  Footer,
+  Sidebar,
+  DashboardLayout,
+  AuthLayout,
+  Toast,
+  GoalCard,
+  PostCard,
+  SocialFeed,
+  GoalList,
+};
